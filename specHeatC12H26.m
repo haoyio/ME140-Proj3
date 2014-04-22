e7d692678ca65d69c0c18d60302afe73a1012f82
haoyio <haoyi@stanford.edu>
2014-04-22 11:36:59 -0700

    final update to remove merge errors

--- specHeatC12H26.m
+++ specHeatC12H26.m
@@ -19,13 +19,6 @@ MF_H2O = 13*phi/M_total;                % molar fraction H2O
 MF_N2 = 3.76*18.5/M_total;              % molar fraction N2
 MF_O2 = 18.5*(1-phi)/M_total;           % molar fraction O2
 MF = [MF_CO2 MF_H2O MF_N2 MF_O2]';      % molar fractions
-=======
-MF_CO2 = 12*phi*const.M_CO2;            % mass fraction CO2
-MF_H2O = 13*phi*const.M_H2O;            % mass fraction H2O
-MF_N2 = 69.56*const.M_N2;               % mass fraction N2
-MF_O2 = 18.5*(1-phi)*const.M_O2;        % mass fraction O2
-MF = [MF_CO2 MF_H2O MF_N2 MF_O2]';
-
 
 M = [const.M_CO2; const.M_H2O; const.M_N2; const.M_O2];     % molar mass
 
@@ -34,10 +27,6 @@ ABCD = [const.abcd_CO2; const.abcd_H2O; ...
 
 
 [cp, cv, k] = specHeatMix(M, MF, ABCD, T);
-
-end
-=======
-[cp, cv, k] = specHeatMix(M, MF, ABCD(:,1), ABCD(:,2), ABCD(:,3), ABCD(:,4), T);
 
 end
 
