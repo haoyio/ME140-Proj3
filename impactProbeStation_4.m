87527d5417c79567e963eaf11a2f3542ec370e52
denalibenner <denalibenner@gmail.com>
2014-04-22 16:59:54 -0700

    Added plots part 1

--- impactProbeStation_4.m
+++ impactProbeStation_4.m
@@ -1,4 +1,4 @@
-function [P, Po, M, V, T, To] = impactProbeStation_4(m_dot, A, Tm, P, RF)
+function [P, Po, M, V, T, To] = impactProbeStation_4(m_dot, A, Tm, P, RF,isVarK)
 % Inputs arrays, outputs arrays
 % Handles all the array complications for different spool speeds
 
@@ -12,25 +12,25 @@ To = zeros(1,numDataPoints);
 
 for i = 1:numDataPoints
     [P(i), Po(i), M(i), V(i), T(i), To(i)] =...
-        station4(m_dot(i), A, Tm(i), P(i), RF);  
+        station4(m_dot(i), A, Tm(i), P(i), RF,isVarK);  
 end
 
 end
 
-function [P, Po, M, V, T, To] = station4(m_dot, A, Tm, P, RF)
+function [P, Po, M, V, T, To] = station4(m_dot, A, Tm, P, RF, isVarK)
 % Iterative procedure to determine MFP, To, M, and T
-
 M = 0;
 T = 0;
 To = Tm;
 Po = P;
-[Cp,Cv,k] = specHeat(Tm);
+if isVarK == true
+    [Cp,Cv,k] = specHeat(T);
+else
+    [Cp,Cv,k] = specHeat(300);
+end
 R = Cp - Cv;
-
 dP = 1;
 dT = 0.01;
-
-
 
 while(true)
     while(true)
@@ -51,12 +51,22 @@ while(true)
         To = To_new;
         break;
     end
-    [Cp,Cv,k] = specHeat(T);
+    
+    if isVarK == true
+        [Cp,Cv,k] = specHeat(T);
+    else
+        [Cp,Cv,k] = specHeat(300);
+    end
     T = Tm / (1+RF*(k-1)/2*M^2);
 
 end
 
-[~,~,k] = specHeat(Tm);
+if isVarK == true
+    [~,~,k] = specHeat(T);
+else
+    [~,~,k] = specHeat(300);
+end
+
 T = Tm / (1+RF*(k-1)/2*M^2);
 V = M * sqrt(k*R*T);
 
--- proj3Main.m
+++ proj3Main.m
@@ -2,43 +2,37 @@
 clear; close all; clc;
 
 loaddata;
-
-% Station 2 conditions (air)
+%%
+% Calculations for air with variable specific heats
 [m_air, P2, Po2, M2, V2, T2, To2] = ...
     impactProbeStation_2(DP2, const.Patm, T2m);
-
-% Station 3 conditions (air)
 [P3, Po3, M3, V3, T3, To3] = ...
-    impactProbeStations_3_5_8(m_air, const.A3, T3m, Po3, const.RF_cr);
-
-% Station 4 conditions (air)
+    impactProbeStations_3_5_8(m_air, const.A3, T3m, Po3, const.RF_cr, 1);
 [P4, Po4, M4, V4, T4, To4] = ...
-   impactProbeStation_4(m_air, const.A4, T4m, P4, const.RF_cr);
-
-% Station 5 conditions (air)
+   impactProbeStation_4(m_air, const.A4, T4m, P4, const.RF_cr, 1);
 [P5, Po5, M5, V5, T5, To5] = ...
-   impactProbeStations_3_5_8(m_air + m_fuel, const.A5, T5m, Po5, const.RF_ax);
-
-% Station 8 conditions (air)
+   impactProbeStations_3_5_8(m_air + m_fuel, const.A5, T5m, Po5, const.RF_ax, 1);
 [P8, Po8, M8, V8, T8, To8] = ...
-    impactProbeStations_3_5_8(m_air + m_fuel, const.A8, T8m, Po8, const.RF_cr);
-
+    impactProbeStations_3_5_8(m_air + m_fuel, const.A8, T8m, Po8, const.RF_cr, 1);
 
 AF = m_air./m_fuel;
-
-% Use adiabatic flame temp to calculate To4
-% Station 4 conditions (combustion products)
+%%
+% Calculations for combustion products
 [P4c, Po4c, M4c, V4c, T4c, To4c] = ...
     productsStation_4(m_air + m_fuel, const.A4, T4m, P4, const.RF_cr, AF);
-
-% Station 5 conditions (combustion products)
 [P5c, Po5c, M5c, V5c, T5c, To5c] = ...
     productsStations_5_8(m_air + m_fuel, const.A5, T5m, Po5, const.RF_ax, AF);
-
-% Station8 conditions (combustion products)
 [P8c, Po8c, M8c, V8c, T8c, To8c] = ...
     productsStations_5_8(m_air + m_fuel, const.A8, T8m, Po8, const.RF_cr, AF);
+%%
+% Calculations for air with constant specific heats
+[P4, Po4, M4, V4, T4, To4] = ...
+   impactProbeStation_4(m_air, const.A4, T4m, P4, const.RF_cr, 1);
+[P5, Po5, M5, V5, T5, To5] = ...
+   impactProbeStations_3_5_8(m_air + m_fuel, const.A5, T5m, Po5, const.RF_ax, 1);
 
+
+%%
 % % PART 4 stuff
 % Generate struct for part 4 function
 in4.To2 = To2; in4.Po2 = Po2;
@@ -59,7 +53,7 @@ out4 = wrapperPart4(in4, length(DP2)); % get eff_NOZ, eff_TUR, eff_CMB, eff_COM,
 [thrust_calc, specific_thrust, TSFC, eta] =...
     part3(m_air+m_fuel, m_fuel, V8c, const.LHV);
 
-
+%%
 plotsPart2(To2,To3,To4,To5,To8,...
     Po2, Po3, Po4c, Po5c, Po8c,...
     M2, M3, M4c, M5c, M8c,...
@@ -69,3 +63,32 @@ plotsPart2(To2,To3,To4,To5,To8,...
 plotsPart3(thrust_calc, thrust, specific_thrust, TSFC, eta);
 
 plotsPart4(in4)
+
+%%
+[workVarCp,workConstCp,workComb] = deal(zeros(1,8));
+for i = 1:8
+    workVarCp(i) = getWork(To4(i), To5(i), m_air(i) + m_fuel(i), 1);
+    workConstCp(i) = getWork(To4(i), To5(i), m_air(i) + m_fuel(i), 0);
+    workCombProd(i) = getWork(To4(i), To5(i), m_air(i) + m_fuel(i), 1);
+end
+
+w = [45:5:80]; % spool speed
+W_to_kW = 1e-3;
+
+% Plot Compressor/Turbine Power
+figure(1)
+h1 = plot(w,workCombProd*W_to_kW,'d-',...
+    w,workVarCp*W_to_kW,'o-',...
+    w,workConstCp*W_to_kW,'*-');
+legend('Combustion Products, Variable Cp',...
+    'Air, Variable Cp',...
+    'Air, Constant Cp',...
+    'Location','NorthWest')
+xlabel('Spool Speed [krpm]')
+ylabel('Power [kW]')
+title('Power Produced/Consumed vs Spool Speed')
+plotfixer;
+ylim([0,45]);
+xlim([45,80]);
+set(h1,'LineWidth',3);
+print('-depsc','-tiff','-r300','PowervsSpeed');
