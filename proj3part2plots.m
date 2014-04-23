3a0e74f2580c1995e15ccebb31097dde6b94a43b
denalibenner <denalibenner@gmail.com>
2014-04-22 23:12:31 -0700

    plots for flame temp vs phi, part 2

---
+++ proj3part2plots.m
@@ -0,0 +1,21 @@
+function proj3part2plots
+phi = [0.05:0.05:0.65];
+
+Tin = 300;
+Tout = zeros(1,length(phi));
+for i = 1:length(phi);
+    Tout(i) = adiabaticFlameTemperaturePhi(phi(i), Tin);
+end
+
+figure(1)
+h1 = plot(phi,Tout);
+xlabel('Equivalence Ratio')
+ylabel('Adiabatic Flame Temperature [K]')
+title('Adiabatic Flame Temperature vs Equivalence Ratio (Air and Jet A fuel')
+plotfixer;
+ylim([250,950]);
+xlim([45,80]);
+set(h1,'LineWidth',3);
+% print('-depsc','-tiff','-r300','TovsSpeed');
+
+end
